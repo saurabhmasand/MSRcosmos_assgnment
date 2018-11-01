560101c00cb38cc9cab6167fd4bb05a5a300e484
saurabhmasand <39308501+saurabhmasand@users.noreply.github.com>
2018-11-01 12:45:56 +0530

    Update droplet.tf

--- one/droplet.tf
+++ one/droplet.tf
@@ -4,28 +4,28 @@ resource "digitalocean_ssh_key" "default" {
   public_key = "${file("/Users/saurabhsingh/.ssh/id_rsa.pub")}"
 }
 
-# Create a new Droplet using the SSH key
-#resource "digitalocean_droplet" "web1" {
-#  image    = "ubuntu-18-04-x64"
-#  name     = "MSR-test-Instance-1"
-#  region   = "blr1"
-#  size     = "s-1vcpu-1gb"
-#  ssh_keys = ["${digitalocean_ssh_key.default.id}"]
-#}
+Create a new Droplet using the SSH key
+resource "digitalocean_droplet" "web1" {
+  image    = "ubuntu-18-04-x64"
+  name     = "MSR-test-Instance-1"
+  region   = "blr1"
+  size     = "s-1vcpu-1gb"
+  ssh_keys = ["${digitalocean_ssh_key.default.id}"]
+}
 
-# Create a another Droplet using the SSH key
-#resource "digitalocean_droplet" "web2" {
-#  image    = "ubuntu-18-04-x64"
-#  name     = "MSR-test-Instance-2"
-#  region   = "blr1"
-#  size     = "s-1vcpu-1gb"
-#  ssh_keys = ["${digitalocean_ssh_key.default.id}"]
-#}
+Create a another Droplet using the SSH key
+resource "digitalocean_droplet" "web2" {
+  image    = "ubuntu-18-04-x64"
+  name     = "MSR-test-Instance-2"
+  region   = "blr1"
+  size     = "s-1vcpu-1gb"
+  ssh_keys = ["${digitalocean_ssh_key.default.id}"]
+}
 
-# Create a new Droplet using the SSH key
+# Create a new Droplet using the SSH key - for Ansible Master
 resource "digitalocean_droplet" "web3" {
   image    = "ubuntu-18-04-x64"
-  name     = "MSR-test-master"
+  name     = "MSR-master"
   region   = "blr1"
   size     = "s-1vcpu-1gb"
   ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]
